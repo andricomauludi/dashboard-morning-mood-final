45f2a787af05dc6a7a950c04a5899ec88fe98fd5
andricomauludi <mauludi21@gmail.com>
2024-09-17 09:46:09 +0700

    mantab

--- src/app/point-of-sales/ReceiptHelper.js
+++ src/app/point-of-sales/ReceiptHelper.js
@@ -15,7 +15,7 @@ const formatDate = (timestamp) => {
     month: "long",
     day: "numeric",
     hour: "2-digit",
-    minute: "2-digit",
+    minute: "2-digit", //
     second: "2-digit",
     timeZone: "Asia/Jakarta", // Set timezone to Indonesia
   };
