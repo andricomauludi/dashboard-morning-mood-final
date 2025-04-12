7dfdd016259448c1f96fb3f04516e338199fa64a
andricomauludi <mauludi21@gmail.com>
2025-04-13 05:47:11 +0700

    give up

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // export const BACKEND="http://20.195.8.233:8090"
 // export const BACKEND="http://20.195.42.22:8090"
 // export const BACKEND="http://103.175.217.209:8090"
-// export const BACKEND="https://backend.ceumonny.com"
+export const BACKEND="https://backend.ceumonny.com"
 // export const BACKEND="http://127.0.0.1:8090"
 
-export const BACKEND = process.env.REACT_APP_BACKEND;
+// export const BACKEND = process.env.REACT_APP_BACKEND;
